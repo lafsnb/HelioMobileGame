3c36d99a2a7ab44af4aff1ae904b92d3f46afb28
Unknown <royalterravore@gmail.com>
2017-04-06 13:41:48 -0600

    Added Enemies
    I also added enemy test scene

---
+++ FinalGame/Assets/Scripts/Enemies/EnemyWeakspot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyWeakspot : MonoBehaviour {
+
+    Enemies parent;
+
+	void Start ()
+    {
+        parent = GetComponentInParent<Enemies>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        parent.HP -= 1;
+
+        if (parent.HP <= 0)
+        {
+            parent.AnimationTrigger("Death");
+        }
+        else
+        {
+            parent.AnimationTrigger("Hit");
+        }
+    }
+}
