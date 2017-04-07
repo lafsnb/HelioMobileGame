4c734adb2144bb34475c8580d67dedccc31a99c7
Anthony Romrell <anthony@artisandream.com>
2017-04-06 19:10:39 -0600

    Added NavMesh

---
+++ FinalGame/Assets/Rotate.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	Vector3 rot = new Vector3(0,1,0);
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rot);
+	}
+}
